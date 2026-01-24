84127d0528b91e2f7f39e9b5fd94b1abfd4717ba
NasusMain <107574485+NasysMeiner@users.noreply.github.com>
2026-01-25 02:20:53 +0300

    LightCreator

---
+++ Assets/Project/Scripts/Nikita/LightCreator.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class LightCreator : MonoBehaviour
+{
+    public GameObject lightObject;  // Перетащите сюда GameObject с Light
+    public float activeTime = 0.05f; // Свет горит 0.05 секунды
+
+    [SerializeField] private ParticleSystem particleSystem;
+    private float timer;
+    private bool lightOn;
+
+    void Awake()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+
+        // Гарантируем, что свет выключен в начале
+        if (lightObject != null)
+        {
+            lightObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        // Если Particle System проигрывается - включаем свет
+        if (particleSystem.isPlaying && particleSystem.particleCount > 0)
+        {
+            if (!lightOn)
+            {
+                TurnOnLight();
+            }
+            timer = activeTime; // Сбрасываем таймер
+        }
+
+        // Таймер выключения
+        if (lightOn)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                TurnOffLight();
+            }
+        }
+    }
+
+    void TurnOnLight()
+    {
+        if (lightObject != null)
+        {
+            lightObject.SetActive(true);
+            lightOn = true;
+        }
+    }
+
+    void TurnOffLight()
+    {
+        if (lightObject != null)
+        {
+            lightObject.SetActive(false);
+            lightOn = false;
+        }
+    }
+
+    void OnDisable()
+    {
+        TurnOffLight();
+    }
+}
