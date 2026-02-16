43f28f691ae518fa5548cc6739c1e39d7052e87a
Slashaershov <127024236+Slashaershov@users.noreply.github.com>
2026-02-17 00:55:06 +0300

    in process

--- Assets/Project/Scripts/Vyacheslav/Weapone.cs
+++ Assets/Project/Scripts/Vyacheslav/Weapone.cs
@@ -2,14 +2,11 @@ using UnityEngine;
 
 public class Weapone : Akila.FPSFramework.Firearm
 {
-    protected override void DoFireDone(Vector3 position, Quaternion rotation, Vector3 direction)
-    {
-        base.DoFireDone(position, rotation, direction);
-        DoShoot(position, rotation, direction);
-    }
-
-    private void DoShoot(Vector3 position, Quaternion rotation, Vector3 direction)
+    protected override void DoSpecialShoot(Vector3 position, Quaternion rotation, Vector3 direction, float speed, float range)
     {
         LightingProjectile projectile = PoolManager.Instance.GetObject<LightingProjectile>();
+        projectile.transform.position = position;
+        projectile.gameObject.SetActive(true);
+        projectile.Setup(direction, speed, range);
     }
 }
