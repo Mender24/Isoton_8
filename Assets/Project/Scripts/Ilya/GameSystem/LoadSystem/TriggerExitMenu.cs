54ab6abf4194d6d96e981feb0788b0d83b396d6c
Mender24 <Nikkvinsi@Gmail.com>
2026-03-04 04:12:04 +0300

    TriggerShower

--- Assets/Project/Scripts/Ilya/GameSystem/LoadSystem/TriggerExitMenu.cs
+++ Assets/Project/Scripts/Ilya/GameSystem/LoadSystem/TriggerExitMenu.cs
@@ -1,9 +1,11 @@
+using Akila.FPSFramework;
 using UnityEngine;
 
 public class TriggerExitMenu : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        SceneLoader.instance.LoadMainMenu();
+        if (other.gameObject.TryGetComponent(out Player _))
+            SceneLoader.instance.LoadMainMenu();
     }
 }
