5eb0e5ea0c07b1516383e9df5973734793346b47
Sinty <mrsinty@gmail.com>
2026-04-21 22:17:12 +0300

    Bot yap only when alert

--- Assets/Project/Scripts/Anatoliy/AI/Enemy/Audio/EnemyYapper.cs
+++ Assets/Project/Scripts/Anatoliy/AI/Enemy/Audio/EnemyYapper.cs
@@ -6,13 +6,31 @@ public class EnemyYapper : MonoBehaviour
     [SerializeField] private float _minInterval = 8f;
     [SerializeField] private float _maxInterval = 20f;
 
+    private EnemyState _state;
     private float _timer;
 
     private void Awake()
     {
         if (_audio == null)
             _audio = GetComponent<EnemyAudioController>();
-        ResetTimer();
+
+        var enemyBase = GetComponent<EnemyBase>();
+        if (enemyBase != null)
+            _state = enemyBase.State;
+    }
+
+    private void Start()
+    {
+        if (_state != null)
+            _state.OnAlertedChanged += OnAlertedChanged;
+
+        enabled = false;
+    }
+
+    private void OnDestroy()
+    {
+        if (_state != null)
+            _state.OnAlertedChanged -= OnAlertedChanged;
     }
 
     private void Update()
@@ -25,13 +43,18 @@ public class EnemyYapper : MonoBehaviour
         }
     }
 
-    public void StartYapping()
+    private void OnAlertedChanged(bool isAlerted)
     {
-        ResetTimer();
-        enabled = true;
+        if (isAlerted)
+        {
+            ResetTimer();
+            enabled = true;
+        }
+        else
+        {
+            enabled = false;
+        }
     }
-
-    public void StopYapping() => enabled = false;
 
     private void ResetTimer() => _timer = Random.Range(_minInterval, _maxInterval);
 }
