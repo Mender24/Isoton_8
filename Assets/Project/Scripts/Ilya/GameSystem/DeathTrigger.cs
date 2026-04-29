df3057543cda61d74da0962f5e5e6373b5e127cd
NasusMain <107574485+NasysMeiner@users.noreply.github.com>
2026-04-29 07:06:23 +0300

    Occlusion fix

--- Assets/Project/Scripts/Ilya/GameSystem/DeathTrigger.cs
+++ Assets/Project/Scripts/Ilya/GameSystem/DeathTrigger.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class DeathTrigger : MonoBehaviour
 {
     [SerializeField] private float _damage = 200f;
+    [SerializeField] private bool _isOnlyPlayer = true;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out IDamageable damageable))
         {
+            if (_isOnlyPlayer && !other.TryGetComponent(out Player player))
+                return;
+
             damageable.Damage(_damage, null);
         }
     }
